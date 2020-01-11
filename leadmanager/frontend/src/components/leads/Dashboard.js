9140ea93ffca44babbb2e46bbc11591c6d94c0dc
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-01-10 19:13:24 -0700

    setting up react and redux

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,23 @@
+import React, {
+    Fragment
+} from 'react';
+import Form from './Form';
+import Leads from './Leads';
+
+
+export default function Dashboard() {
+    return ( <
+        div >
+        <
+        Fragment >
+        <
+        Leads >
+
+        <
+        /Leads> <
+        /Fragment> <
+        /div>
+    )
+}
+
+export default Dashboard;
--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('frontend.urls')),  # frontend has to load before the leads.urls
     path('', include('leads.urls')),
 ]
